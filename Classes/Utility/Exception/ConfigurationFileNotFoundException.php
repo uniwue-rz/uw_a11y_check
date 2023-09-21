493f843dd8c1ddf6ac4447a13e187d25174d32dd
Torben Hansen <derhansen@gmail.com>
2023-09-21 13:00:51 +0200

    [TASK] CGL fixes

--- Classes/Utility/Exception/ConfigurationFileNotFoundException.php
+++ Classes/Utility/Exception/ConfigurationFileNotFoundException.php
@@ -7,4 +7,6 @@ namespace UniWue\UwA11yCheck\Utility\Exception;
 /**
  * Exception thrown if the configuration yaml file is not found
  */
-class ConfigurationFileNotFoundException extends \RuntimeException {}
+class ConfigurationFileNotFoundException extends \RuntimeException
+{
+}
--- Classes/Utility/Exception/MissingConfigurationException.php
+++ Classes/Utility/Exception/MissingConfigurationException.php
@@ -7,4 +7,6 @@ namespace UniWue\UwA11yCheck\Utility\Exception;
 /**
  * Exception thrown if the configuration does not contain an expected entry
  */
-class MissingConfigurationException extends \RuntimeException {}
+class MissingConfigurationException extends \RuntimeException
+{
+}
