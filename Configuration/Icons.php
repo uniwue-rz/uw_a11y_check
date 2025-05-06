c19bbc84d58126a1cbe38b82f5bf0383762b3b71
Torben Hansen <derhansen@gmail.com>
2025-05-06 16:36:01 +0200

    [BUGFIX] Add missing backend module icon
    Refs #8

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'ext-uwa11ycheck-default' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:uw_a11y_check/Resources/Public/Icons/Extension.svg',
+    ],
+];
